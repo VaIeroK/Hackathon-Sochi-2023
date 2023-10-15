f0ac06b0ec8153661f368826e0ef4b118de8f3ba
VaIeroK <kondratenko449@gmail.com>
2023-10-15 05:54:56 +0300

    wip

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import overpy
-import json
 import asyncio
+import json
+import os
 from math import sin, cos, sqrt, atan2, radians
 from decimal import Decimal
 from map import structures_searchbysity
 from map import get_city_districts
+from map import convert_decimal_to_float
 from map import RegionInfo
 
-region_structures = { "school", "kindergarten", "language_school", "music_school", "college", "university", "driving_school", "training" }
-city_structures = { "theme_park", "zoo", "national_park", "park", "water_park", "trampoline_park", "fountain", "place_of_worship" }
-positive_structures = { "civic", "stadium", "riding_hall", "sports_hall", "cycleway", "ice_rink", "footway", "pitch", "track", "marketplace", "greengrocer", "farm" }
+region_structures = { "school", "kindergarten", "language_school", "music_school", "college", "university", "driving_school", "training", "driving_school", "dancing_school" }
+city_structures = { "theme_park", "zoo", "national_park", "park", "water_park", "trampoline_park", "fountain", "place_of_worship", "playground" }
+positive_structures = { "civic", "stadium", "riding_hall", "sports_hall", "cycleway", "ice_rink", "footway", "pitch", "track", "marketplace", "greengrocer", "farm", "playground" }
 negative_structures = { "tobacco", "kiosk", "alcohol", "pub", "fast_food", "food_court", "bar", "biergarten", "beverages", "wine" }
 
 def distance(lat1, lon1, lat2, lon2):
@@ -39,7 +40,8 @@ def get_distances_of_type(data, src_type, dest_type_list, max_radius = -1):
                         dist = distance(structure["Широта"], structure["Долгота"], dest_structure["Широта"], dest_structure["Долгота"])
                         if dist < max_radius or max_radius == -1:
                             dist_dict = { "Тип": dest_type, "МейнНазвание": structure["Название"], "Название": dest_structure["Название"], "Дистанция": round(dist, 2) }
-                            distances.append(dist_dict)
+                            if not dist_dict in distances:
+                                distances.append(dist_dict)
     return distances
 
 def get_structures_count(data, type_list):
@@ -53,6 +55,14 @@ def get_structures_count(data, type_list):
 def print_distances(main_structure, distances):
     for dist in distances:
         print(f"Дистанция {dist["Тип"]} до {main_structure}: {dist["Дистанция"]} км")
+        
+def calculate_factor(value):
+    if value <= 0.1:
+        return 0
+    elif value >= 0.2:
+        return 1
+    else:
+        return (value - 0.1) / (0.2 - 0.1)
 
 async def parse_city(city):
     live_prompt = []
@@ -61,49 +71,104 @@ async def parse_city(city):
     negative_distances_count = 0
     data = structures_searchbysity(city)
     
+    if not os.path.exists(f"app_cache\\results\\{city}"):
+        os.makedirs(f"app_cache\\results\\{city}")
+    
     for main_structure in city_structures:
-        negative_dists = get_distances_of_type(data, main_structure, negative_structures, 0.3)
+        negative_dists = get_distances_of_type(data, main_structure, negative_structures, 0.4)
         for dist in negative_dists:
             average_negative_distance += dist["Дистанция"]
         negative_distances_count += len(negative_dists)
         #print_distances(main_structure, negative_dists)
-    average_negative_distance = average_negative_distance / negative_distances_count
-        
-    print(f"Средняя дистанция негативных объектов до парков поблизости: {round(average_negative_distance, 2)} км")
-    if average_negative_distance > 0.2:
-        live_quality = live_quality + 30
+    if negative_distances_count != 0:
+        average_negative_distance = average_negative_distance / negative_distances_count
     else:
-        live_prompt.append(f"Необходимо ограничить парки от табачной и алкогольной продукции. Средняя дистанция до парков: {average_negative_distance}")    
+        average_negative_distance = None
+        
+    if average_negative_distance != None:
+        print(f"Средняя дистанция негативных объектов до парков поблизости: {round(average_negative_distance, 2)} км")
+        factor = calculate_factor(average_negative_distance)
+        live_quality = live_quality + (30 * factor)
+        if factor < 0.2:
+            live_prompt.append(f"Необходимо ограничить парки от табачной и алкогольной продукции. Средняя дистанция до парков: {average_negative_distance}")    
+        
     rayons = get_city_districts(city)
+    
     positive_rayons = 0
     negative_rayons = 0
     
     tasks = []
     for rayon_name in rayons:
-        tasks.append(parse_region(rayon_name, live_prompt))
+        tasks.append(parse_region(city, rayon_name, live_prompt))
     results = await asyncio.gather(*tasks)
     
+    rayons_info = []
     for rayon_data in results:
+        rayons_info.append({
+            "Название": rayon_data.reg_name,
+            "Баллы": rayon_data.live_quality
+        })
         if rayon_data.positive_structures_count > 1.5 * rayon_data.negative_structures_count:
             positive_rayons += 1
         else:
             negative_rayons += 1
             live_prompt.append(f"В {rayon_data.reg_name} положительные структуры не преобладают на 50% над негативными структурами: {rayon_data.positive_structures_count} vs {1.5 * rayon_data.negative_structures_count}")
 
-    diff = positive_rayons - negative_rayons
-    if diff >= 1:
-        live_quality = live_quality + 70
+    with open(f"app_cache\\results\\{city}\\rayons.json", "w", encoding="utf-8") as json_file:
+        json.dump(rayons_info, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
+
+    live_quality = live_quality + (70 * (positive_rayons / len(rayons)))
         
-    print(f"Хороших районов {diff} из {len(rayons)}")
+    print(f"Хороших районов {positive_rayons} из {len(rayons)}")
     print(f"Качество жизни города {city}: {live_quality}")
+
+    with open(f"app_cache\\results\\{city}\\prompt.json", "w", encoding="utf-8") as json_file:
+        json.dump(live_prompt, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
+        
+    with open(f"app_cache\\results\\{city}\\info.json", "w", encoding="utf-8") as json_file:
+        linfo = {
+            "LifeQuality": live_quality,
+            "Name": city,
+            "PositiveRayons": positive_rayons,
+            "AllRayons": len(rayons)
+        }
+        json.dump(linfo, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
+        
     print("prompt:")
     for prompt in live_prompt:
         print(prompt)
         
-async def parse_region(city, live_prompt):
+def parse_saved_data(city):
+    linfo = []
+    prompt = []
+    rayons = []
+    
+    if os.path.exists(f"app_cache\\results\\{city}\\rayons.json"):
+        with open(f"app_cache\\results\\{city}\\rayons.json", 'r', encoding='utf-8') as f:
+            rayons = json.load(f)
+            
+    for rayon in rayons:
+        print(f"Качество жизни района {rayon["Название"]}: {rayon["Баллы"]}")        
+    
+    if os.path.exists(f"app_cache\\results\\{city}\\info.json"):
+        with open(f"app_cache\\results\\{city}\\info.json", 'r', encoding='utf-8') as f:
+            linfo = json.load(f)
+            
+    if os.path.exists(f"app_cache\\results\\{city}\\prompt.json"):
+        with open(f"app_cache\\results\\{city}\\prompt.json", 'r', encoding='utf-8') as f:
+            live_prompt = json.load(f)
+            
+    print(f"Хороших районов {linfo["PositiveRayons"]} из {linfo["AllRayons"]}")
+    print(f"Качество жизни города {city}: {linfo["LifeQuality"]}")
+    print("prompt:")
+    for prompt in live_prompt:
+        print(prompt)
+    
+        
+async def parse_region(city_name, rayon, live_prompt):
     reg_quality_struct_count = []
     live_quality = 0
-    data = structures_searchbysity(city)
+    data = structures_searchbysity(rayon, city_name)
     
     positive_count = get_structures_count(data, positive_structures)
     negative_count = get_structures_count(data, negative_structures)
@@ -129,12 +194,11 @@ async def parse_region(city, live_prompt):
         #print(f"Добро и позитив! {positive_count} vs {negative_count}")
     #else:
         #print(f"Зло и негатив! {positive_count} vs {negative_count}")
-    print(f"Качество жизни района {city}: {live_quality}")
-    return RegionInfo(city, positive_structs_count, reg_quality_struct_count, live_quality, positive_count, negative_count)
+    print(f"Качество жизни района {rayon}: {live_quality}")
+    
+    return RegionInfo(rayon, positive_structs_count, reg_quality_struct_count, live_quality, positive_count, negative_count)
     
 #print(len(tobacco_searchbysity("Сочи")))
-asyncio.run(parse_city("Ростов-на-Дону"))
+#asyncio.run(parse_city("Ростов-на-Дону"))
+parse_saved_data("Ростов-на-Дону")
 #print(get_city_districts("Советский район"))
-    
-
-    
--- map.py
+++ map.py
@@ -1,11 +1,13 @@
 import overpy
 import json
 import os
+import httpx
+import asyncio
 from decimal import Decimal
 
-region_structures = { "school", "kindergarten", "language_school", "music_school", "college", "university", "driving_school", "training" }
-city_structures = { "theme_park", "zoo", "national_park", "park", "water_park", "trampoline_park", "fountain", "place_of_worship" }
-positive_structures = { "civic", "stadium", "riding_hall", "sports_hall", "cycleway", "ice_rink", "footway", "pitch", "track", "marketplace", "greengrocer", "farm" }
+region_structures = { "school", "kindergarten", "language_school", "music_school", "college", "university", "driving_school", "training", "driving_school", "dancing_school" }
+city_structures = { "theme_park", "zoo", "national_park", "park", "water_park", "trampoline_park", "fountain", "place_of_worship", "playground" }
+positive_structures = { "civic", "stadium", "riding_hall", "sports_hall", "cycleway", "ice_rink", "footway", "pitch", "track", "marketplace", "greengrocer", "farm", "playground" }
 negative_structures = { "tobacco", "kiosk", "alcohol", "pub", "fast_food", "food_court", "bar", "biergarten", "beverages", "wine" }
 
 class RegionInfo:
@@ -31,69 +33,154 @@ def load_cache(Name):
             return data
     else:
         return None
+    
+def playgrounds_searchbysity(Name):
+    api = overpy.Overpass()
+
+    data = []
+    osm_query = f"""
+    [out:json][timeout:250];
+    area["name"="{Name}"]->.searchArea;
+    nwr["leisure"="playground"](area.searchArea);
+    out center;
+    """
+
+    result = api.query(osm_query)
+
+    for way in result.ways:
+        struct_type = way.tags.get("leisure", "Не указано")
+        if not struct_type in region_structures and not struct_type in city_structures:
+            continue
+        struct_name = way.tags.get("name", "null")
+        if struct_name == "null":
+            continue
+        data.append({
+            "Тип": struct_type,
+            "Название": struct_name,
+            "Широта": way.center_lat,
+            "Долгота": way.center_lon
+        })
+    return data
 
 def parks_searchbysity(Name):
     api = overpy.Overpass()
-    data = load_cache(Name)
     
-    if data == None: 
-        data = []
-        osm_query = f"""
-        [out:json][timeout:250];
-        area["name"="{Name}"]->.searchArea;
-        way(area.searchArea)["leisure"];
-        out center;
-        """
+    data = []
+    osm_query = f"""
+    [out:json][timeout:250];
+    area["name"="{Name}"]->.searchArea;
+    way(area.searchArea)["leisure"];
+    out center;
+    """
 
-        result = api.query(osm_query)
+    result = api.query(osm_query)
 
-        for way in result.ways:
-            struct_type = way.tags.get("leisure", "Не указано")
-            if not struct_type in region_structures and not struct_type in city_structures:
-                continue
-            data.append({
-                "Тип": struct_type,
-                "Широта": way.center_lat,
-                "Долгота": way.center_lon
-            })
-        with open(f"app_cache\\{Name}.json", "w", encoding="utf-8") as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
+    for way in result.ways:
+        struct_type = way.tags.get("leisure", "Не указано")
+        if not struct_type in region_structures and not struct_type in city_structures:
+            continue
+        struct_name = way.tags.get("name", "null")
+        if struct_name == "null":
+            continue
+        data.append({
+            "Тип": struct_type,
+            "Название": struct_name,
+            "Широта": way.center_lat,
+            "Долгота": way.center_lon
+        })
     return data
 
-def tobacco_searchbysity(Name):
+def highway_searchbysity(Name):
     api = overpy.Overpass()
-    data = load_cache(Name)
-    
-    if data == None: 
-        data = []
-        osm_query = f"""
-        [out:json][timeout:250];
-        area["name"="{Name}"]->.searchArea;
-        (
-        nwr["shop"="tobacco"](area.searchArea);
-        nwr["shop"="kiosk"](area.searchArea);
-        );
-        out center;
-        """
+    data = []
+    osm_query = f"""
+    [out:json][timeout:250];
+    area["name"="{Name}"]->.searchArea;
+    way(area.searchArea)["highway"];
+    out center;
+    """
 
-        result = api.query(osm_query)
+    result = api.query(osm_query)
 
-        for way in result.ways:
-            struct_name = way.tags.get("name", "null")
-            if struct_name == "null":
-                continue
-            data.append({
-                "Тип": "tobacco",
-                "Название": struct_name,
-                "Широта": way.center_lat,
-                "Долгота": way.center_lon
-            })
-        with open(f"app_cache\\{Name}.json", "w", encoding="utf-8") as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
-
+    for way in result.ways:
+        struct_type = way.tags.get("highway", "Не указано")
+        if not struct_type in region_structures and not struct_type in city_structures and not struct_type in positive_structures and not struct_type in negative_structures:
+            continue
+        struct_name = way.tags.get("name", "null")
+        if struct_name == "null":
+            continue
+        data.append({
+            "Тип": struct_type,
+            "Название": struct_name,
+            "Широта": way.center_lat,
+            "Долгота": way.center_lon
+        })
     return data
 
-def structures_searchbysity(Name):
+def fetch_tobacco_shops(city, skip, results_per_page, page, results):
+    api_key = "4deae437-2d92-4cb6-a8aa-79e3754c8f35"
+    base_url = "https://search-maps.yandex.ru/v1/"
+
+    search_params = {
+        "text": f"{city}, табака",
+        "type": "biz",
+        "lang": "ru_RU",
+        "results": results_per_page,
+        "apikey": api_key,
+        "skip": skip
+    }
+
+    with httpx.Client() as client:
+        response = client.get(base_url, params=search_params)
+        data = response.json()
+
+    if "features" in data:
+        for feature in data["features"]:
+            name = feature["properties"]["CompanyMetaData"]["name"]
+            coordinates = feature["geometry"]["coordinates"]
+
+            result = {
+                "Тип": "tobacco",
+                "Название": "Помогите(((((( (табачка какая то лол)",
+                "Широта": coordinates[1],  
+                "Долгота": coordinates[0],
+            }
+
+            results.append(result)
+
+def tobacco_searchbysity(Name, data):
+    api = overpy.Overpass()
+    
+    osm_query = f"""
+    [out:json][timeout:250];
+    area["name"="{Name}"]->.searchArea;
+    (
+    nwr["shop"="tobacco"](area.searchArea);
+    );
+    out center;
+    """
+
+    result = api.query(osm_query)
+
+    for way in result.ways:
+        struct_name = way.tags.get("name", "null")
+        if struct_name == "null":
+            continue
+        data.append({
+            "Тип": "tobacco",
+            "Название": struct_name,
+            "Широта": way.center_lat,
+            "Долгота": way.center_lon
+        })
+            
+    num_pages = 10
+    results_per_page = 50
+    
+    for page in range(1, num_pages + 1):
+        skip = (page - 1) * results_per_page
+        fetch_tobacco_shops(Name, skip, results_per_page, page, data)
+
+def structures_searchbysity(Name, city_name = None):
     api = overpy.Overpass()
     data = load_cache(Name)
     
@@ -112,16 +199,21 @@ def structures_searchbysity(Name):
             struct_type = way.tags.get("amenity", "Не указано")
             if not struct_type in region_structures and not struct_type in city_structures and not struct_type in positive_structures and not struct_type in negative_structures:
                 continue
+            struct_name = way.tags.get("name", "null")
+            if struct_name == "null":
+                continue
             data.append({
                 "Тип": struct_type,
-                "Название": way.tags.get("name", "Без названия"),
+                "Название": struct_name,
                 "Широта": way.center_lat,
                 "Долгота": way.center_lon
             })
-        with open(f"app_cache\\{Name}.json", "w", encoding="utf-8") as json_file:
+        tobacco_searchbysity(Name, data)
+        data.extend(parks_searchbysity(Name))
+        data.extend(highway_searchbysity(Name))
+        data.extend(playgrounds_searchbysity(Name))
+        with open(f"app_cache\\{f"{city_name}_" if city_name != None else ""}{Name}.json", "w", encoding="utf-8") as json_file:
             json.dump(data, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
-    data.extend(tobacco_searchbysity(Name))
-    data.extend(parks_searchbysity(Name))
     return data
 
 def get_city_districts(city_name, admlevel=8):
@@ -149,3 +241,32 @@ def get_city_districts(city_name, admlevel=8):
             json.dump(district_names, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
 
     return district_names
+
+def search_cemeteries_in_city(city):
+    api = overpy.Overpass()
+
+    osm_query = f"""
+    [out:json][timeout:25];
+    area["name"="{city}"]->.searchArea;
+    (
+     way["landuse"="cemetery"](area.searchArea);
+    );
+    out center;
+    """
+
+    result = api.query(osm_query)
+
+    cemeteries = []
+
+    for way in result.ways:
+        lat = way.center_lat
+        lon = way.center_lon
+        name = way.name
+        cemeteries.append({
+            "Долгота": lat,
+            "Ширина": lon,
+            "Имя": name
+        })
+
+    with open(f"{city}_cemeteries.json", "w", encoding="utf-8") as json_file:
+        json.dump(cemeteries, json_file, ensure_ascii=False, indent=4, default=convert_decimal_to_float)
